b45163229cc1c444e44ffc30749bc26728bbadd0
IevgeniiGlagoliev <glagolev.dev@gmail.com>
2024-06-12 02:23:33 +0300

    finish slider

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ const embedEngine = {
     binds() {
         const swiper = new Swiper('.swiper', {
             slidesPerView: 1,
+            loop: true,
             pagination: {
                 el: '.swiper-pagination',
+                clickable: true,
             },
             navigation: {
                 nextEl: '.swiper-button-next',
